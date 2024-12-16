208b22e06a4054a3be8e8bb7df2774dca312b55f
JordanRondon <jordanangelrondonpozo@gmail.com>
2024-12-16 11:09:51 -0500

    feat(CircularDoublyLinkedList): development of class methods for the list

--- ConsoleMusicPlayer/CircularDoublyLinkedList.cpp
+++ ConsoleMusicPlayer/CircularDoublyLinkedList.cpp
@@ -1,33 +1,109 @@
 #include "CircularDoublyLinkedList.h"
+#include <string> 
 
 CircularDoublyLinkedList::CircularDoublyLinkedList() :
-	CircularList(nullptr) {
+	list(nullptr) {
 }
 
 Node* CircularDoublyLinkedList::getList() {
-	return NULL;
+	return this->list;
 }
 
 void CircularDoublyLinkedList::insertEnd(Music musicInf) {
+	
+	Node* newNode = new Node;
+	Node* lastNode;
+
+	newNode->MusicObj = musicInf;
+	newNode->next = list;
+
+	if (list == NULL) {
+		lastNode = newNode;
+	}
+	else {
+		lastNode = list->previous;
+		list->previous = newNode;
+	}
+
+	list = newNode;
+	lastNode->next = list;
+	list->previous = lastNode;
+
+	list = list->next;
 
 }
 
-void CircularDoublyLinkedList::deletePosition(int position) {
+void CircularDoublyLinkedList::deletePosition(int index) {
+	
+	Node* currentNode = list;
+	int currentPosition = 0;
+
+	if (list != NULL) {
+		if (list->previous == list && list->next == list) {
+			list = NULL;
+			return;
+		}
+
+		do {
+			currentPosition++;
+			if (currentPosition < index)
+				currentNode = currentNode->next;
+		} while (currentPosition < index && currentNode != list);
+
+		if (currentNode->previous == list->previous) {//elimina el primer nodo
+			//verifica si el dato a eliminar esta en el inicio de lista
+			list = currentNode->next;//actualiza una posicion adelante el inicio de lista
+			list->previous = currentNode->previous;
+			list->previous->next = list;
+		}
+		else if (currentNode->next == list) {//elimina el ultimo nodo
+			//verifica si el dato a eliminar esta en el final de lista
+			currentNode->previous->next = list;
+			list->previous = currentNode->previous;
+		}
+		else {//elimina en cualquier posición
+			currentNode->previous->next = currentNode->next;
+			currentNode->next->previous = currentNode->previous;
+		}
+
+		delete currentNode;
+	}
 
 }
 
-void CircularDoublyLinkedList::searchElement(std::string musicName) {
+bool CircularDoublyLinkedList::searchElement(std::string musicName) {
+	Node* currentNode = list;
 
+	if (list != NULL) {
+		do {
+			if (currentNode->MusicObj.getName() == musicName)
+				return true;
+			currentNode = currentNode->next;
+		} while (currentNode != list);
+	}
+
+	return false;
 }
 
-void CircularDoublyLinkedList::count() {
+int CircularDoublyLinkedList::count() {
+	
+	Node* currentNode = list;
+	int totalItems = 0;
 
+	if (list != NULL) {
+		do {
+			totalItems++;
+			currentNode = currentNode->next;
+		} while (currentNode != list);
+	}
+
+	return totalItems;
 }
 
 Node* CircularDoublyLinkedList::next() {
-	return NULL;
+	return list->next;
 }
 
 Node* CircularDoublyLinkedList::before() {
-	return NULL;
+	return list->previous;
 }
