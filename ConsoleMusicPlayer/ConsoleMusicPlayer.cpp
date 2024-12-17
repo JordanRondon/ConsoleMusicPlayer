f38a2d80e620b9b5afa1b5e6ab4896b72722b75f
JordanRondon <jordanangelrondonpozo@gmail.com>
2024-12-17 17:04:40 -0500

    style(style): playlist box styles

--- ConsoleMusicPlayer/ConsoleMusicPlayer.cpp
+++ ConsoleMusicPlayer/ConsoleMusicPlayer.cpp
@@ -1,50 +1,47 @@
 #include <iostream>
 #include "MusicPlayer.h"
+#include "style.h"
 
 using namespace std;
 
 int main() {
-    
-    //MusicPlayer player;
 
-    //player.load("C:/Users/JORDAN/Music/asd.mp3");
-    //player.play();
+    musicBox(4, 70, 25, 2);
+    listBox(10, 70, 25, 8);
+    gotoxy(2, 22);
 
-    //// Espera por la entrada del usuario para detener la música
-    //std::cin.get();
+    //try {
+    //    MusicPlayer player;
 
-    try {
-        MusicPlayer player;
+    //    // Ruta del archivo MP3
+    //    const char* filePath = "C:/Users/JORDAN/Music/asd.mp3";
 
-        // Ruta del archivo MP3
-        const char* filePath = "C:/Users/JORDAN/Music/asd.mp3";
+    //    player.load(filePath);
+    //    player.play();
 
-        player.load(filePath);
-        player.play();
+    //    // Espera por la entrada del usuario para controlar la música
+    //    char input;
+    //    while (true) {
+    //        std::cout << "Comandos: (f) Adelantar 5s, (b) Retroceder 5s, (q) Detener\n";
+    //        std::cin >> input;
 
-        // Espera por la entrada del usuario para controlar la música
-        char input;
-        while (true) {
-            std::cout << "Comandos: (f) Adelantar 5s, (b) Retroceder 5s, (q) Detener\n";
-            std::cin >> input;
+    //        if (input == 'f') {
+    //            player.seekForward(); // Adelantar 5 segundos
+    //        }
+    //        else if (input == 'b') {
+    //            player.seekBackward(); // Retroceder 5 segundos
+    //        }
+    //        else if (input == 'q') {
+    //            player.stop(); // Detener la reproducción
+    //            break;
+    //        }
+    //    }
 
-            if (input == 'f') {
-                player.seekForward(); // Adelantar 5 segundos
-            }
-            else if (input == 'b') {
-                player.seekBackward(); // Retroceder 5 segundos
-            }
-            else if (input == 'q') {
-                player.stop(); // Detener la reproducción
-                break;
-            }
-        }
-
-    }
-    catch (const std::exception& e) {
-        std::cerr << "Excepción: " << e.what() << std::endl;
-        return 1;
-    }
+    //}
+    //catch (const std::exception& e) {
+    //    std::cerr << "Excepción: " << e.what() << std::endl;
+    //    return 1;
+    //}
 
     return 0;
 }
---
+++ ConsoleMusicPlayer/style.h
@@ -0,0 +1,6 @@
+#pragma once
+
+void gotoxy(int posX, int posY);
+void musicBox(int height, int width, int posX, int posY);
+void listBox(int height, int width, int posX, int posY);
+void barraDeProgreso(int width, int posX, int posY);
