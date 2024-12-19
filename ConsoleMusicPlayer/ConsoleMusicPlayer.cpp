416049333ba8eca412d4a3e26a38f91cc29b6e48
JordanRondon <jordanangelrondonpozo@gmail.com>
2024-12-19 18:40:40 -0500

    feat(FileUtils): module to obtain information from files

--- ConsoleMusicPlayer/ConsoleMusicPlayer.cpp
+++ ConsoleMusicPlayer/ConsoleMusicPlayer.cpp
@@ -6,6 +6,7 @@
 #include "CircularDoublyLinkedList.h"
 #include "MusicData.h"
 #include "MusicPlayer.h"
+#include "FileUtils.h"
 #include "style.h"
 
 #define QUIT 113        // 'q'
@@ -20,9 +21,7 @@
 
 using namespace std;
 
-string getUserMusicPath();
-void loadFileMp3(const char* ruta, CircularDoublyLinkedList& list, MusicPlayer& musicP);
-void threadNexSong(MusicPlayer& MusicP, CircularDoublyLinkedList& list, string& currentMusicPath, std::atomic<bool>& running);
+void threadNextSong(MusicPlayer& MusicP, CircularDoublyLinkedList& List, string& currentMusicPath, std::atomic<bool>& running);
 
 int main() {
 
@@ -31,33 +30,33 @@ int main() {
     int boxPosX = 25;
     int boxPosY = 7;
 
-    CircularDoublyLinkedList list;
+    CircularDoublyLinkedList List;
     MusicPlayer MusicP;
 
     int opcionLoadFile = filePathOption(boxHeight, boxWidth, boxPosX, boxPosY);
 
     switch (opcionLoadFile) {
     case 1:
-        loadFileMp3(getUserMusicPath().c_str(), list, MusicP);
+        loadFileMp3(getUserMusicPath().c_str(), List, MusicP);
         break;
     case 2:
         string carpeta = filePathString(boxHeight, boxWidth, boxPosX, boxPosY);
-        loadFileMp3(carpeta.c_str(), list, MusicP);
+        loadFileMp3(carpeta.c_str(), List, MusicP);
         break;
     }
         
-    boxHeight = list.count();
+    boxHeight = List.count();
     boxPosY = 8;
     char keyPress = NULL;
     string currentMusicPath = "";
 
     std::atomic<bool> running(true);
-    std::thread threadMusic (threadNexSong, std::ref(MusicP), std::ref(list), std::ref(currentMusicPath), std::ref(running));
+    std::thread threadMusic (threadNextSong, std::ref(MusicP), std::ref(List), std::ref(currentMusicPath), std::ref(running));
 
     do {
-        musicListInterface(boxHeight, boxWidth, boxPosX, boxPosY, list, MusicP);
+        musicListInterface(boxHeight, boxWidth, boxPosX, boxPosY, List, MusicP);
 
-        string musicPath = getUserMusicPath() + '/' + list.getCurrentNode()->MusicObj.getName();
+        string musicPath = getUserMusicPath() + '/' + List.getCurrentNode()->MusicObj.getName();
         std::replace(musicPath.begin(), musicPath.end(), '\\', '/');
         gotoxy(0, (boxPosY + boxHeight + 6));
 
@@ -94,10 +93,10 @@ int main() {
             MusicP.volumeDown();
             break;
         case NEXT:
-            list.nextNode();
+            List.nextNode();
             break;
         case PREVIOUS:
-            list.previousNode();
+            List.previousNode();
             break;
         }
         system("CLS");
@@ -109,47 +108,11 @@ int main() {
     return 0;
 }
 
-string getUserMusicPath() {
-    char* userProfile = nullptr;
-    size_t len = 0;
-
-    if (_dupenv_s(&userProfile, &len, "USERPROFILE") == 0 && userProfile != nullptr) {
-        std::string musicPath = std::string(userProfile) + "\\Music";
-        free(userProfile);
-        return musicPath;
-    }
-
-    return "";
-}
-
-void loadFileMp3(const char* path, CircularDoublyLinkedList& list, MusicPlayer& musicP) {
-    Music musicData;
-
-    if (!std::filesystem::exists(path)) {
-        std::cerr << "Error: The route '" << path << "' does not exist." << std::endl;
-        return;
-    }
-
-    if (!std::filesystem::is_directory(path)) {
-        std::cerr << "Error: '" << path << "' is not a valid directory." << std::endl;
-        return;
-    }
-
-    for (auto const& dir_entry : filesystem::directory_iterator(path)) {
-        if (dir_entry.is_regular_file() && dir_entry.path().extension() == ".mp3") {
-            musicData.setName(dir_entry.path().filename().string());
-            musicP.load(dir_entry.path().string().c_str());
-            musicData.setDurationSeconds(musicP.getDuration());
-            list.insertEnd(musicData);
-        }
-    }
-}
-
-void threadNexSong(MusicPlayer& MusicP, CircularDoublyLinkedList& list, string& currentMusicPath, std::atomic<bool>& running) {
+void threadNextSong(MusicPlayer& MusicP, CircularDoublyLinkedList& List, string& currentMusicPath, std::atomic<bool>& running) {
     while (running) {
         if (MusicP.isFinished()) {
-            list.nextNode();
-            string musicPath = getUserMusicPath() + '/' + list.getCurrentNode()->MusicObj.getName();
+            List.nextNode();
+            string musicPath = getUserMusicPath() + '/' + List.getCurrentNode()->MusicObj.getName();
             std::replace(musicPath.begin(), musicPath.end(), '\\', '/');
             MusicP.load(musicPath.c_str());
             currentMusicPath = musicPath;
