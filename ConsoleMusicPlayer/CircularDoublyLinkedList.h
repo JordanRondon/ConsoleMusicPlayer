d5f9d17046032cd727b16ea47f91656c2d604abf
JordanRondon <jordanangelrondonpozo@gmail.com>
2024-12-18 22:51:43 -0500

    feat(CircularDoublyLinkedList): Next and Previous Node Function

--- ConsoleMusicPlayer/CircularDoublyLinkedList.h
+++ ConsoleMusicPlayer/CircularDoublyLinkedList.h
@@ -5,13 +5,17 @@
 class CircularDoublyLinkedList {
 	private:
 		Node* list;
+		Node* currentNode;
 
 	public:
 		CircularDoublyLinkedList();
 
 		Node* getList();
+		Node* getCurrentNode();
 		void insertEnd(Music musicInf);
 		void deletePosition(int position);
 		bool searchElement(std::string musicName);
 		int count();
+		void nextNode();
+		void previousNode();
 };
