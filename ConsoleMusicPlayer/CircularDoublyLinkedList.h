9a9a2164134461be6e005b6c26c6d47b747c13b9
JordanRondon <jordanangelrondonpozo@gmail.com>
2024-12-20 16:22:23 -0500

    docs(CircularDoublyLinkedList): Documentation of the methods of the circular list class

--- ConsoleMusicPlayer/CircularDoublyLinkedList.h
+++ ConsoleMusicPlayer/CircularDoublyLinkedList.h
@@ -13,8 +13,6 @@ class CircularDoublyLinkedList {
 		Node* getList();
 		Node* getCurrentNode();
 		void insertEnd(Music musicInf);
-		void deletePosition(int position);
-		bool searchElement(std::string musicName);
 		int count();
 		void nextNode();
 		void previousNode();
